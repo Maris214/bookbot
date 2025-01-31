f9af14a3a0782d48289d54e7206bef9b6d349a07
Maris214 <dmrodriguez1021@gmail.com>
2025-01-31 10:23:18 -0500

    main code of project

---
+++ main.py
@@ -0,0 +1,44 @@
+def main(book_path):
+    with open(book_path) as f:
+        return f.read()
+
+def count_words(text):
+    count = len(text.split())
+    return count
+
+def count_characters(text):
+    characters = {}
+    for letter in text:
+        characters.setdefault(letter.lower(), 0)
+        characters[letter.lower()] += 1
+    return characters
+
+def get_alphabet(text):
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    alpha_char = {}
+    for letter in all_characters:
+        if letter in alphabet:
+            alpha_char[letter] = all_characters[letter]
+    return alpha_char
+
+def print_report(text):
+    print(f"""
+    --- Begin report of books/frankenstein.txt ---
+    {words} words found in the document
+    """)
+    for item in alpha_characters:
+        print(f"The '{item}' character was found {alpha_characters[item]} times")
+    print("--- End report ---")
+    return
+
+book = main("books/frankenstein.txt")
+words = count_words(book)
+all_characters = count_characters(book)
+alpha_characters = get_alphabet(book)
+
+print_report(book)
+
+
+
+
+
